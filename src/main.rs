3b9f2010d97d001b8595eda86c5e5ac61c6973e0
Danish Kalesaran <danishkalesaran@gmail.com>
2024-02-19 13:49:39 +0800

    feat: pong by ping count

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
@@ -6,13 +9,27 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                println!("accepted new connection");
-                let buf = b"+PONG\r\n";
-                stream.write_all(buf).expect("Failed to write to client.");
+                let mut message = String::new();
+                stream
+                    .read_to_string(&mut message)
+                    .expect("Failed to read from client.");
+                pong(&mut stream, ping_count(&message));
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+}
+
+fn ping_count(message: &str) -> usize {
+    let by_newline: Vec<&str> = message.trim().split('\n').collect();
+    by_newline.len()
+}
+
+fn pong(stream: &mut TcpStream, count: usize) {
+    let buf = b"+PONG\r\n";
+    for _i in 0..count {
+        stream.write_all(buf).expect("Failed to write to client.");
     }
 }
