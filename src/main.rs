1eefbaa567e5d4aa714dcd8a07e22cb638aeedd6
Danish Kalesaran <danishkalesaran@gmail.com>
2024-02-19 11:32:32 +0800

    feat: reply PONG

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                let buf = b"+PONG\r\n";
+                stream.write_all(buf).expect("Failed to write to client.");
             }
             Err(e) => {
                 println!("error: {}", e);
